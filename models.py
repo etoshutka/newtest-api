241728cc42587638e386ce3b084cbe6ff3e856d6
etoshutka <cheatmaster114@gmail.com>
2024-08-15 15:24:11 +0500

    s

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy import Column, Integer, DateTime, BigInteger
 from sqlalchemy.sql import func
 from database import Base
+
+
 
 class Referral(Base):
     __tablename__ = "referrals"
     id = Column(Integer, primary_key=True, index=True)
-    user_tg_id = Column(Integer, index=True)
-    friend_tg_id = Column(Integer, index=True)
+    user_tg_id = Column(BigInteger, index=True)
+    friend_tg_id = Column(BigInteger, index=True)
     date = Column(DateTime, default=func.now())
     points = Column(Integer, default=100)
